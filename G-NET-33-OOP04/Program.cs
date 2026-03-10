24d3b43e02f549eb874315af2aa6600bbaf69dfc
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-03-11 00:51:14 +0300

    Solution to the first section

--- G-NET-33-OOP04/Program.cs
+++ G-NET-33-OOP04/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿#region Part 1
+
+// Q1 : What is the difference between static binding and dynamic binding? When does each one happen?
+
+/*
+static :Overloading, Compile Time ,Same Method name but Different Signature ; Dynamic : Overriding , Runtime ,Same Method name and same Signature
+*/
+
+// Q2 What is the difference between method overloading and method overriding?
+
+/*
+
+-- Overloading : It happens in The same Class 
+-- Overriding : It happens Between Basic class and derived class 
+
+*/
+
+
+// Q3 : What keywords are used for Method Overriding? What does each one mean ?
+
+/*
+
+-- we useing virtual Keyword in the basic Class To be Overridng 
+-- we useing override Keyword int the Derived Class to provide a new implementation of the base method 
+
+*/
+
+
+    
+#endregion
